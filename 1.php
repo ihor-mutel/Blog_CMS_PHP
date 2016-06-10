1afb7696ff78cd5cbb6b236baa072bab37fb8146
web1991t <web1991t@gmail.com>
2016-06-10 07:55:43 +0300

    practice section 1, write echo function and comment

---
+++ 1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+   echo "<h1>Hello student</h1>";
+   
+    // function say hello
+?>
+
+</body>
+</html>
