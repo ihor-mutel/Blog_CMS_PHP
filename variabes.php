fa3ca605c2945269411ebb0518ed9a91fb0bae47
web1991t <web1991t@gmail.com>
2016-06-10 11:12:31 +0300

    vairable syntax

--- variabes.php
+++ variabes.php
@@ -5,7 +5,26 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+$name = "Edwin";
+$name2 = 'Edwin';
+$number = 100;
 
+echo $name;
+    
+//$num-ber (error)
+//$0number (error)
+    
+//Two different variables    
+$NUMBER = 100;
+$NUMBer = 100;    
+
+//string    
+$makingString = "100";   
+    
+    
+    
+?>
         
 </body>
 </html>
