fb1bac8e67193de6d0498610d1f3f76bc2890034
web1991t <web1991t@gmail.com>
2016-06-10 13:57:44 +0300

    if statement syntax

--- ifstatement.php
+++ ifstatement.php
@@ -6,7 +6,14 @@
 </head>
 <body>
 <?php
-
+if (3 > 10) {
+    echo "three is less than tan";
+} elseif( 4 > 5) {
+    echo "of course four is less than five";
+} else {
+    echo "it is not";
+}
+    
 ?>
 </body>
 </html>
