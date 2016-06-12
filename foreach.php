ae72134265609a68bae7b2f960f5441064d5821c
web1991t <web1991t@gmail.com>
2016-06-12 18:57:32 +0300

    foreach loop syntax

--- foreach.php
+++ foreach.php
@@ -6,7 +6,16 @@
 </head>
 <body>
 <?php
+//$numbers - array
+//in each iteration current item will be assing to $number
 
+$numbers = array(345,397,676,385,2657,5784);
+    
+foreach($numbers as $number){
+    
+echo $number . "<br>";
+    
+}
 ?>
 </body>
 </html>
