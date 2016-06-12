189ea7d45a314276f0f34d75826e2d8b9c0feb36
web1991t <web1991t@gmail.com>
2016-06-12 20:08:59 +0300

    returning values example

--- return_values_functions.php
+++ return_values_functions.php
@@ -6,7 +6,20 @@
 </head>
 <body>
 <?php
+    
+function addNumbers($number1, $number2){
+$sum = $number1 + $number2;
+    return $sum;
+}
+$result = addNumbers(34,64); //assign what function return
 
+echo $result; //show result (98)
+echo "<br>";    
+
+$result = addNumbers(100,$result); //reuse the same function
+
+echo $result; //show result again (198)
+    
 ?>
 </body>
 </html>
