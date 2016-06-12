7de40410551485c2bbb283bd7826cade330dd72c
web1991t <web1991t@gmail.com>
2016-06-12 20:32:22 +0300

    constant syntax

--- constants.php
+++ constants.php
@@ -7,6 +7,13 @@
 <body>
 <?php
 
+$number = 10;
+echo $number . "<br>";  
+    
+define("NAME", 1000); //define constant (name of constant, value)
+echo NAME; //gives us 1000
+    
+//NAME = 1000 gives us error because you can't change constant  
 ?>
 </body>
 </html>
