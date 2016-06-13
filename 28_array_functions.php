1c3895c75ab6e07c6f9e2243d3d132f8593c6a78
web1991t <web1991t@gmail.com>
2016-06-13 20:07:54 +0300

    examples of array functions

--- 28_array_functions.php
+++ 28_array_functions.php
@@ -6,6 +6,18 @@
 </head>
 <body>
 <?php
+$list = [343,34,323,23,54,232,453];
+
+echo max($list); //gives us max value in array (253)
+echo "<br>";
+
+echo min($list); //gives us min value in array (23)
+echo "<br>";
+
+print_r($list); //print the whole array
+echo "<br>";
+
+sort($list); //sort values
 
 ?>
 </body>
