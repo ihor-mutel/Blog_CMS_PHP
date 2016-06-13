0e5667860e414f6134f1e7e691244b4c53fd632a
web1991t <web1991t@gmail.com>
2016-06-13 20:17:58 +0300

    create form

--- form.php
+++ form.php
@@ -5,8 +5,10 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-
-?>
+<form action="form.php" method="post">
+    <input type="text" placeholder="Enter Username">
+    <input type="password" placeholder="Enter Password"><br>
+    <input type="submit">    
+</form>
 </body>
 </html>
