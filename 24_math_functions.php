af55833b43b43a13a834fceb35ca2e70d7ea60cd
web1991t <web1991t@gmail.com>
2016-06-13 19:52:00 +0300

    example of math funtions

--- 24_math_functions.php
+++ 24_math_functions.php
@@ -6,7 +6,17 @@
 </head>
 <body>
 <?php
-
+echo pow(2,2); //gives us 4
+echo "<br>";
+echo rand(1,1000); //gives us random number
+echo "<br>";
+echo sqrt(100); //gives us 10
+echo "<br>";
+echo ceil(4.5); //gives us 5
+echo "<br>";
+echo floor(4.5); //gives us 4 
+echo "<br>";
+echo round(4.5); //gives us 5 again
 ?>
 </body>
 </html>
