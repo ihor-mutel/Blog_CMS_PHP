63d90bc8e149bb648078e304669d97eadde0b04b
web1991t <web1991t@gmail.com>
2016-06-14 19:53:24 +0300

    check if user name in array or not

--- form.php
+++ form.php
@@ -2,6 +2,7 @@
 if(isset($_POST['submit'])){
 $username = $_POST['username'];
 $password = $_POST['password'];
+$name = array("Edwin", "Student", "Petir", "Mohad", "Jane", "Tom");
 $minimum = 5;
 $maximum = 10;
 
@@ -12,6 +13,12 @@ $maximum = 10;
     if(strlen($username) > $maximum ){
         echo "Username can't be longer than 10";
         
+    }
+    
+    if(!in_array($username, $name)){ //in_array built in PHP function
+        echo "Sorry yu are not allowed";
+    }else {
+        echo "Welcome";
     }
 }
 ?>
