b84f1697c2f71ced0b8f3a721c69a18789df7549
web1991t <web1991t@gmail.com>
2016-07-13 14:34:20 +0300

    remove annoying text from db.php

--- mysql/db.php
+++ mysql/db.php
@@ -1,8 +1,6 @@
 <?php
 $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
-    if ($connection) {
-        echo "We are connected";        
-    }else {
+    if (!$connection) {
         die("Database connection failed");        
     }
 
