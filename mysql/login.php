5b749c70ad675c38d6d76850b5096d9dcde4bbb1
web1991t <web1991t@gmail.com>
2016-07-13 11:34:23 +0300

    create form with bootstrap

--- mysql/login.php
+++ mysql/login.php
@@ -6,6 +6,21 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
-    
+<div class="container">
+    <div class="col-xs-6"> <!-- grid class -->
+        <form action="">
+           
+            <div class="form-group">
+               <label for="username">Username</label>
+                <input type="text" name="username" class="form-control">
+            </div>
+            
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div>            
+        </form>
+    </div>    
+</div>    
 </body>
 </html>
