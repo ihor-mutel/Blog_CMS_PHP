2836d7e2b840f124ee7ff69d848b66c553c18e0d
web1991t <web1991t@gmail.com>
2016-07-13 16:20:29 +0300

    015 DELETE RECORDS FROM DATABASE WITH PHP, login_update.php save as login_delete.php

---
+++ mysql/login_delete.php
@@ -0,0 +1,46 @@
+<?php include "db.php"; ?>
+<?php include "functions.php"; ?>
+<?php
+
+if(isset($_POST['submit'])) {
+    updateTable();
+}    
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="col-xs-6"> <!-- grid class -->
+        <form action="login_update.php" method="post">
+           
+            <div class="form-group">
+               <label for="username">Username</label>
+                <input type="text" name="username" class="form-control">
+            </div>
+            
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control">
+            </div> 
+            <div class="form-group">
+                <select name="id" id="">
+                    <?php
+                         showAllData();
+                    ?>                  
+                   
+                </select>
+            </div>
+            <input class="btn btn-primary" type="submit" name="submit" value="UPDATE">        
+        </form>
+    </div>      
+</div>    
+</body>
+</html>
