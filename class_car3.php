e3b8d08b985dd78cb90c233aad3eb424efcd592c
web1991t <web1991t@gmail.com>
2016-07-14 14:04:23 +0300

    how to create object and call a method

--- class_car3.php
+++ class_car3.php
@@ -5,7 +5,9 @@ class Car {
             echo "Wheels move";
     }
 }
+$bmw = new Car(); //create object 
+$merces_benz = new Car();
 
-
-
+$bmw->MoveWheels(); // use method of the object
+$merces_benz->MoveWheels();
 ?>
