23f5ba90d588616428db22e2408a3727d0a0a83b
web1991t <web1991t@gmail.com>
2016-07-15 13:56:20 +0300

    make connection, and make parameters as constants

--- cms/includes/db.php
+++ cms/includes/db.php
@@ -1,8 +1,20 @@
 <?php
 
+$db['db_host'] = "localhost"; //add each parameter for connection to a database into an array
+$db['db_user'] = "root";
+$db['db_pass'] = "";
+$db['db_name'] = "cms";
 
+foreach($db as $key => $value){ //go through an array $db
+    //define("CONSTANT", "Hello world.");
+    define(strtoupper($key),$value); //make each parameter in $db constant
+}
 
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
+if ($connection) {
+    echo "We are connected";
+}
 
 
 
