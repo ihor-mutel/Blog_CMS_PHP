79a7f5e43df89036d5f192cf114464dc0c75df01
web1991t <web1991t@gmail.com>
2016-07-15 11:15:16 +0300

    009 STATIC DATA IN CLASSES, class_car3.php save as class_static.php

---
+++ class_static.php
@@ -0,0 +1,22 @@
+<?php
+
+class Car {
+    var $wheels = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 4;
+    function MoveWheels(){
+            $this->wheels = 10;
+    }
+    function CreateDoors(){
+            $this->doors = 6;
+    }
+}
+$bmw = new Car(); //make an object
+$truck  = new Car(); 
+echo $bmw->wheels . "<br>"; 
+
+echo $truck->wheels = 10 . "<br>"; //a truck has another amount of wheels
+$truck->CreateDoors(); //call a method
+echo $truck->doors;  //display a value
+?>
