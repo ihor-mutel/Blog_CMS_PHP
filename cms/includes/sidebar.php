4bf8f46a755a19fc1e416275eb2797016ab5ad24
web1991t <web1991t@gmail.com>
2016-07-17 13:56:14 +0300

    012 CREATING A CUSTOM SEARCH ENGINE PART 2, create search function template and display it

--- cms/includes/sidebar.php
+++ cms/includes/sidebar.php
@@ -2,8 +2,18 @@
 <?php
   
   if(isset($_POST['submit'])) {
-     echo $_POST['search']; 
-      
+     $search = $_POST['search']; 
+     $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' "; 
+     $search_query = mysqli_query($connection, $query);
+     if(!$search_query) {
+         die("QUERY FAILED " . mysqli_error($connection));
+     } 
+     $count = mysqli_num_rows($search_query);
+     if($count == 0){
+         echo "<h1>NO RESULT</h1>";
+     }else{
+         echo "SOME RESULT";
+     }
   }
 ?>
                 <!-- Blog Search Well -->
