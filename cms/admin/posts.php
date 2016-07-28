4bf33454567e549b109c4954704d9161738dd0ac
web1991t <web1991t@gmail.com>
2016-07-28 14:33:06 +0300

    add title and create a table

--- cms/admin/posts.php
+++ cms/admin/posts.php
@@ -9,7 +9,39 @@
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
-
+                        <h1 class="page-header">
+                            Welcome to Admin Page
+                            <small>Author</small>
+                        </h1>
+<table class="table table-bordered table-hover">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Author</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Status</th>
+            <th>Image</th>
+            <th>Tags</th>
+            <th>Comments</th>
+            <th>Date</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>10</td>
+            <td>Edwin Diaz</td>
+            <td>Bootstap Framework</td>
+            <td>Bootstap</td>
+            <td>Status</td>
+            <td>Image</td>
+            <td>Tags</td>
+            <td>Comments</td>
+            <td>Date</td>
+        </tr>
+    </tbody>
+</table>
+                  
                    
                    
                    
