3d18e9d479c6e5b5ece38d6f0d13ec2312673ec8
web1991t <web1991t@gmail.com>
2016-07-28 14:25:24 +0300

    delete all unnecessary code

--- cms/admin/posts.php
+++ cms/admin/posts.php
@@ -9,51 +9,12 @@
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Welcome to Admin Page
-                            <small>Author</small>
-                        </h1>
-                        <div class="col-xs-6">
-<?php
-insert_categories();                        
-?>
-                        
-                        
 
-                        <form action="" method="post">
-                            <div class="form-group">
-                                <label for="cat_title">Add category</label>
-                                <input class="form-control" type="text" name="cat_title">
-                            </div>                            
-                            <div class="form-group">
-                                <input  class="btn btn-primary" type="submit" name="submit" value="Add Category">
-                            </div>   
-                        </form> 
-<?php //UPDATE AND INCLUDE QUERY
-if (isset($_GET['edit'])) {
-$cat_id = $_GET['edit'];
-include "includes/update_categories.php";
-}
-?>                         
-                        </div>
-                        <div class="col-xs-6">
-                        <table class="table table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Category title</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-<?php findAllCategories();?>
-
-<?php deleteCategories(); ?>
-
-                                </tr>
-                            </tbody>
-                        </table>
-                        </div>
+                   
+                   
+                   
+                   
+                   
                     </div>
                 </div>
                 <!-- /.row -->
