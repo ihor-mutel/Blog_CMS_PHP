22011e0480a318debc0cbf318ecd8a73800dfc71
web1991t <web1991t@gmail.com>
2016-07-28 17:15:44 +0300

    002 DISPLAYING POSTS LIST IN ADMIN, add posts properties into table dinamiclly

--- cms/admin/posts.php
+++ cms/admin/posts.php
@@ -28,7 +28,35 @@
         </tr>
     </thead>
     <tbody>
-        <tr>
+<?php 
+       $query = "SELECT * FROM posts"; //you could limit it using "LIMIT"
+    $select_posts = mysqli_query($connection,$query);    
+
+
+    while ($row =  mysqli_fetch_assoc($select_posts)){
+    $post_id = $row['post_id']; // cat_id equals to a field in a database
+    $post_author = $row['post_author'];
+    $post_title = $row['post_title'];   
+    $post_category_id = $row['post_category_id'];   
+    $post_status = $row['post_status'];   
+    $post_image = $row['post_image'];   
+    $post_tags = $row['post_tags'];   
+    $post_comment_count = $row['post_comment_count'];
+    $post_date = $row['post_date'];
+        
+    echo "<tr>";
+    echo "<td>{$post_id}</td>";
+    echo "<td>{$post_author}</td>";
+    echo "<td>{$post_title}</td>";
+    echo "<td>{$post_category_id}</td>";
+    echo "<td>{$post_status}</td>";
+    echo "<td><img width='100' src='../images/{$post_image}' alt='image'></td>";
+    echo "<td>{$post_tags}</td>";
+    echo "<td>{$post_comment_count}</td>";
+    echo "<td>{$post_date}</td>";
+    echo "</tr>";
+    }
+?>        
             <td>10</td>
             <td>Edwin Diaz</td>
             <td>Bootstap Framework</td>
@@ -38,7 +66,7 @@
             <td>Tags</td>
             <td>Comments</td>
             <td>Date</td>
-        </tr>
+        
     </tbody>
 </table>
                   
