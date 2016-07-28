dd2558901f95096237a07b2bbcb5c68b25a3ae35
web1991t <web1991t@gmail.com>
2016-07-28 18:42:58 +0300

    save data from form into variables, and use function move_uploaded_file() in order to save up image

--- cms/admin/includes/add_posts.php
+++ cms/admin/includes/add_posts.php
@@ -1,4 +1,25 @@
-<form action="" method="posts" enctype="multipart/form-data">
+<?php
+    if(isset($_POST['create_post'])){
+        $post_title = $_POST['title']; //explanation at 6:00
+        $post_author = $_POST['author'];
+        $post_post_category_id = $_POST['post_category_id'];
+        $post_post_status = $_POST['post_status'];
+        
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+        
+        $post_tags = $_POST['post_tags'];
+        $post_content = $_POST['post_content'];
+        $post_date = date('d-m-y');
+        $post_comment_count = 4;
+        
+        move_uploaded_file($post_image_temp, "../images/$post_image");
+    }
+?>
+
+   
+
+<form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post Title</label>
         <input type="text" class="form-control" name="title">
@@ -16,8 +37,8 @@
         <input type="text" class="form-control" name="post_status">
     </div>
     <div class="form-group">
-        <label for="post_image">Post Image</label>
-        <input type="file" class="form-control" name="post_image">
+        <label for="image">Post Image</label>
+        <input type="file" class="form-control" name="image">
     </div>
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
